5d4551b9be553ee5f5ff7f8b5888baa4daadaf4e
kent-desktop <kenton1101i@gmail.com>
2024-08-26 22:01:32 +0900

    ViewModel on Compose

--- ViewModel_and_Compose_Sapmple/app/build.gradle.kts
+++ ViewModel_and_Compose_Sapmple/app/build.gradle.kts
@@ -45,4 +45,18 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Compose
+    // https://developer.android.com/jetpack/androidx/releases/compose-foundation?hl=ja#structure
+    implementation("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.ui:ui-tooling:1.6.8")
+    implementation("androidx.compose.foundation:foundation:1.6.8")
+    implementation("androidx.compose.animation:animation:1.6.8")
+    implementation("androidx.compose.material:material:1.6.8")
+    implementation("androidx.compose.material:material-icons-core:1.6.8")
+    // setContentがNotFoundになるので
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation(kotlin("script-runtime"))
+    //material3
+    implementation("androidx.compose.material3:material3:1.2.1")
 }
