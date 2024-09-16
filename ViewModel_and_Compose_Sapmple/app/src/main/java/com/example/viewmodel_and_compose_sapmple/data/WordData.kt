3cc2d0089e322977763d0c6e336dbe3a05ff154e
kent-desktop <kenton1101i@gmail.com>
2024-09-16 22:00:35 +0900

    言葉のリストの追加

---
+++ ViewModel_and_Compose_Sapmple/app/src/main/java/com/example/viewmodel_and_compose_sapmple/data/WordData.kt
@@ -0,0 +1,24 @@
+package com.example.viewmodel_and_compose_sapmple.data
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// ゲームに出てくる言葉のリスト
+
+val allWords: Set<String> =
+    setOf(
+        "animal",   "auto",     "anecdote",
+        "alphabet", "all",      "awesome",
+        "doorknob", "dine",     "dream",
+        "dusk",     "eating",   "elephant",
+        "emerald",  "lily",     "magazine",
+        "marine",   "marshmallow", "maze",
+        "meditate", "melody",   "minute",
+        "style",    "sunrise",  "taxi",
+        "tidy",     "timer",    "together",
+        "tooth",    "tourist",  "you",
+        "year",     "yummy",    "zebra",
+        "zigzag",   "zoology",
+        "zone",     "zeal"
+    )
+
