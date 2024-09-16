83e35bde8e58740addb0d63b43121a65ee0d5b22
kent-desktop <kenton1101i@gmail.com>
2024-09-16 22:33:54 +0900

    modify ViewModel

--- ViewModel_and_Compose_Sapmple/app/src/main/java/com/example/viewmodel_and_compose_sapmple/ui/GameViewModel.kt
+++ ViewModel_and_Compose_Sapmple/app/src/main/java/com/example/viewmodel_and_compose_sapmple/ui/GameViewModel.kt
@@ -2,21 +2,56 @@ package com.example.viewmodel_and_compose_sapmple.ui
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.viewmodel_and_compose_sapmple.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel: ViewModel() {
+    init {
+        resetGame()
+    }
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
     private val _uiState = MutableStateFlow(GameUiState())
 
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
 
     data class GameUiState(
-        val currentWord: String = ""
+        val currentScrambledWord: String = ""
     )
+
+    private fun pickRandomWordAndShuffle(): String {
+        // allWordsからランダムに選んで、シャッフルする
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    // Stringをシャッフルする
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
 
     private val _count = 0
 
     val count: Int
         get() = _count
 }
+
